de6320c212651eb56b768ea46e94b8ad88404937
yooveloper <yooveloper@gmail.com>
2025-05-29 17:12:26 +0900

    add og

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,29 @@ interface Props {
 const Home: NextPage<Props> = ({ postList }) => {
   return (
     <>
-      <NextSeo title={`${BLOG_TITLE} blog`} />
+      <NextSeo
+        title={`${BLOG_TITLE} blog`}
+        description='프론트엔드 개발자 유원영 블로그'
+        canonical='https://yooveloper.dev'
+        openGraph={{
+          type: 'website',
+          url: 'https://yooveloper.dev/',
+          title: `${BLOG_TITLE} blog`,
+          description: '프론트엔드 개발자 유원영의 블로그입니다.',
+          images: [
+            {
+              url: 'https://yooveloper.dev/og-image.png',
+              width: 1200,
+              height: 630,
+              alt: '대표 이미지',
+            },
+          ],
+          site_name: `${BLOG_TITLE} blog`,
+        }}
+        twitter={{
+          cardType: 'summary_large_image',
+        }}
+      />
       <List posts={postList} />
     </>
   );
